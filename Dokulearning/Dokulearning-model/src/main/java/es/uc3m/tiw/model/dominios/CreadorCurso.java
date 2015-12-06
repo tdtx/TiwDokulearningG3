fd46573e8fbc84518c53865f52ca8339ee238492
tdtx <tomasdomingo@hotmail.com>
2015-12-07 00:28:20 +0100

    p

--- Dokulearning/Dokulearning-model/src/main/java/es/uc3m/tiw/model/dominios/CreadorCurso.java
+++ Dokulearning/Dokulearning-model/src/main/java/es/uc3m/tiw/model/dominios/CreadorCurso.java
@@ -8,11 +8,13 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+@Entity
 public class CreadorCurso implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
